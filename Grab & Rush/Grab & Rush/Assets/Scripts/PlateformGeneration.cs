9a8ae7485ae7983a2f8eb09a91694f8dc1e5a6d4
DESKTOP-0ERJPHV\Neko <bernard.sourichanh@gmail.com>
2018-06-27 12:22:11 +0200

    Add Sprite

---
+++ Grab & Rush/Grab & Rush/Assets/Scripts/PlateformGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformGeneration : MonoBehaviour {
+    public GameObject Plateform;
+    public Transform generationPoint; // les plateformes ce crées
+    public float distanceBetween;
+
+    private float plateformWidth = 1.5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        plateformWidth = Plateform.GetComponent<BoxCollider2D>().size.x;
+        Debug.Log("navi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
