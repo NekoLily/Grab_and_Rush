70770a59acc6f25158134f3ae360cd10a1b0a34e
DESKTOP-1KAV99S\cleme <clement.lavieville@gmail.com>
2018-06-27 11:02:28 +0200

    ajout scene

---
+++ Grab & Rush/Grab & Rush/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class GameManager : MonoBehaviour {
+
+    public Enum.GameState GameState;
+    public int Score, ScoreLimit; //Variable du score, limite du score fixée par les joueurs
+
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    private void Awake()
+    {
+        GameState = Enum.GameState.MainMenu; //Lance le menu principal quand on allume le jeu
+    }
+    void Start () {
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (GameState) //Permet de gérer le jeu selon son état GameState
+        {
+            case Enum.GameState.MainMenu: //Menu principal
+                break;
+            case Enum.GameState.BallSelect: //Selection de la boule/Skin, score max
+                break;
+            case Enum.GameState.GamePhaseP1Run: //Phase de jeu, le joueur 1 court
+                break;
+            case Enum.GameState.GamePhaseP2Run: //Phase de jeu, le joueur 2 court
+                break;
+            case Enum.GameState.MenuPause: //Menu pause en jeu
+                break;
+            case Enum.GameState.EndRound: //Fin du round, phase de transition et d'ajout au score
+                break;
+            case Enum.GameState.Victory://Fin de la partie, afficher le score et recommencer/arreter de jouer
+                break;
+            case Enum.GameState.Credits: //Generique
+                break;
+            default:
+                break;
+        }
+	}
+}
---
+++ Grab & Rush/Grab & Rush/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    // Use this for initialization
+    public int Score;
+
+    public int Offset_Increase_Score;
+    public int Offset_Decrease_Score;
+
+    Text _ScoreText;
+
+
+    void Start()
+    {
+        _ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+    }
+
+    public void IncreaseScore()
+    {
+        Score += Offset_Increase_Score;
+        _ScoreText.text = "Score : " + Score;
+    }
+
+    public void DecreaseScore(int num)  //Décremente le score en fonction de l'event.
+    {
+        if ((Score + Offset_Decrease_Score) < 0)
+            Score = 0;
+        else
+            switch (num)
+            {
+                case 1:
+                    Score += Offset_Decrease_Score;
+                    break;
+
+                case 2:
+                    Score += Offset_Decrease_Score;
+                    break;
+
+                case 3:
+                    Score += Offset_Decrease_Score;
+                    break;
+
+            }
+        _ScoreText.text = "Score : " + Score;
+    }
+}
