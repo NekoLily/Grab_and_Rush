09ba872e915d3979457720d3191bd76c6b01bc7e
DESKTOP-0ERJPHV\Neko <bernard.sourichanh@gmail.com>
2018-06-28 22:04:52 +0200

    Fix + Score + Retry

---
+++ Grab & Rush/Grab & Rush/Assets/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public void LoadGame()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().LoadScene(2);
+    }
+
+    public void LoadMainMenu()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().LoadScene(0);
+    }
+}
--- Grab & Rush/Grab & Rush/Assets/Scripts/GameManager.cs
+++ Grab & Rush/Grab & Rush/Assets/Scripts/GameManager.cs
@@ -22,6 +22,8 @@ public class GameManager : MonoBehaviour
     private GameObject Player;
     private GameObject CHENILLE;
     private GameObject Hook;
+    private GameObject P1;
+    private GameObject P2;
     // Use this for initialization
     private void Awake()
     {
@@ -135,7 +137,11 @@ public class GameManager : MonoBehaviour
                 SceneManager.LoadScene("Grab&Rush");
                 StartCoroutine("SpawnPlayer");
                 break;
+
             case 3:
+                SceneManager.LoadScene("Score");
+                break;
+            case 4:
                 GameState = Enum.GameState.Credits;
                 SceneManager.LoadScene("Credits");
                 break;
@@ -144,19 +150,10 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnPlayer()
     {
-
-       
         yield return new WaitForSeconds(0.5f);
-
-        foreach (GameObject GO in GameObject.FindGameObjectsWithTag("Ground"))
-        {
-            Destroy(GO);
-
-        }
+        CHENILLE = Instantiate(Resources.Load<GameObject>("Prefab/LACHENILLE"));
         if (P1Run == false)
-        {
-            
-            CHENILLE = Instantiate(Resources.Load<GameObject>("Prefab/LACHENILLE"));
+        {       
             Player = Instantiate(Resources.Load<GameObject>("Prefab/Player/Player_" + Data[0, 0]));
             Hook = Instantiate(Resources.Load<GameObject>("Prefab/Hook/Hook_" + Data[1, 1]));
 
@@ -168,8 +165,6 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            
-            CHENILLE = Instantiate(Resources.Load<GameObject>("Prefab/LACHENILLE"));
             Player = Instantiate(Resources.Load<GameObject>("Prefab/Player/Player_" + Data[1, 0]));
             Hook = Instantiate(Resources.Load<GameObject>("Prefab/Hook/Hook_" + Data[0, 1]));
 
@@ -182,16 +177,11 @@ public class GameManager : MonoBehaviour
         GameObject.Find("Spawn").GetComponent<PlatformSpawn>().Spawn();
     }
 
-    public void Resume()
-    {
-        GameObject.Find("Menu").SetActive(false);
-        GameState = TMP;
-        Time.timeScale = 1f;
-    }
-
     IEnumerator EndRoundCR()
     {
         EndRoundCRRunning = true;
+        foreach (GameObject GO in GameObject.FindGameObjectsWithTag("Ground"))
+            Destroy(GO);
         DestroyObject(Hook);
         DestroyObject(Player);
         if (ScoreAdded == false)
@@ -211,25 +201,48 @@ public class GameManager : MonoBehaviour
                     ScoreP1 += 1;
             }
             EndRound.text = ScoreP1.ToString() + " - " + ScoreP2.ToString();
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(1.5f);
             EndRound.text = "";
             if (ScoreP1 == ScoreLimit || ScoreP2 == ScoreLimit)
             {
+                LoadScene(3);
+                StartCoroutine("SetScore");
                 GameState = Enum.GameState.Victory;
             }
             else if (P1Run)
             {
                 StartCoroutine("SpawnPlayer");
-                //GameState = Enum.GameState.GamePhaseP2Run;
             }
             else if (P1Run == false)
             {
                 StartCoroutine("SpawnPlayer");
-                //GameState = Enum.GameState.GamePhaseP1Run;
             }
 
             ScoreAdded = true;
         }
         EndRoundCRRunning = false;
+    }
+
+    IEnumerator SetScore()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GameObject.Find("ScoreText").GetComponent<Text>().text = ScoreP1 + " - " + ScoreP2;
+        if (ScoreP1 > ScoreP2)
+        {
+            P1 = Instantiate(Resources.Load<GameObject>("Prefab/Player_Skin/Player_" + Data[0, 0]), new Vector3(-2.5f, -2.2f, 0), transform.rotation);
+            P1.transform.localScale += P1.transform.localScale * 0.5f;
+            P2 = Instantiate(Resources.Load<GameObject>("Prefab/Player_Skin/Player_" + Data[1, 0]), new Vector3(1.5f, -1.5f, 0), transform.rotation);
+            P2.transform.localScale -= P1.transform.localScale * 0.5f;
+        }
+        else
+        {
+            P1 = Instantiate(Resources.Load<GameObject>("Prefab/Player_Skin/Player_" + Data[0, 0]), new Vector3(-2.5f, -1.5f, 0), transform.rotation);
+            P1.transform.localScale -= P1.transform.localScale * 0.5f;
+            P2 = Instantiate(Resources.Load<GameObject>("Prefab/Player_Skin/Player_" + Data[1, 0]), new Vector3(1.5f, -2.2f, 0), transform.rotation);
+            P2.transform.localScale += P1.transform.localScale * 0.5f;
+        }
+        ScoreP1 = 0;
+        ScoreP2 = 0;
+        P1Run = false;
     }
 }
