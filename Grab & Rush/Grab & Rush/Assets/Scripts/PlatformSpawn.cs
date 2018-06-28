88a64524c3539412eeabb3c9818d64168477688e
DIDIPC\Didi <sogiitagunhaguts@gmail.com>
2018-06-28 11:31:12 +0200

    PlateformesFonctionnent

--- Grab & Rush/Grab & Rush/Assets/Scripts/PlatformSpawn.cs
+++ Grab & Rush/Grab & Rush/Assets/Scripts/PlatformSpawn.cs
@@ -4,39 +4,30 @@ using UnityEngine;
 
 public class PlatformSpawn : MonoBehaviour {
 
-    public GameObject PlatformPrefab;
-    public float spawnRate = 3f;
-    public int SizePlatforms = 5;
-    public float PlatformMax = 2.5f;
-    public float PlatformMin = 1.5f;
+    public GameObject[] PlatformPrefab;
+    public float spawnMax = 2.5f;
+    public float spawnMin = 0.5f;
+    public float speed = 5f;
+    
 
-    GameObject[] platforms;
-    int currentPlatforms;
+    //GameObject[] platforms;
+    //int currentPlatforms;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        platforms = new GameObject[SizePlatforms];
-        for(int i=0; i< SizePlatforms; i ++)
-        {
-            platforms[i] = (GameObject)Instantiate(PlatformPrefab, new Vector3(-15.0f, -25.0f, 0), Quaternion.identity);
-        }
-        StartCoroutine("SpawnLoop");
+       Spawn();
+        
 	}
-	
-    IEnumerator SpawnLoop()
+ 
+    void Spawn()
     {
-        while (true)
-        {
-            Vector3 pos = transform.position;
-            pos.y = Random.Range(PlatformMax,PlatformMin);
-            platforms[currentPlatforms].transform.position = pos;
-        }
+        Instantiate(PlatformPrefab[Random.Range(0, PlatformPrefab.GetLength(0))], transform.position, Quaternion.identity);
+        
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+
     }
 
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+    // Update is called once per frame
+   
 }
