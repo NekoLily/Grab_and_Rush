93ceffd16bc6ba2721e83fe6474e2902ca87c8f4
DIDIPC\Didi <sogiitagunhaguts@gmail.com>
2018-06-28 10:41:56 +0200

    Timer+ConditionsDeVictoire

--- Grab & Rush/Grab & Rush/Assets/Scripts/CountDown.cs
+++ Grab & Rush/Grab & Rush/Assets/Scripts/CountDown.cs
@@ -5,24 +5,44 @@ using UnityEngine.UI;
 using System.Threading;
 public class CountDown : MonoBehaviour
 {
-    public int timeLeft = 60;
+    public int timeLeft = 30;
+    GameManager GM;
+    bool CRRunning = false;
     public Text countdown;
     void Start()
     {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
         StartCoroutine("LoseTime");
         Time.timeScale = 1;
     }
     void Update()
     {
-        countdown.text = ("" + timeLeft);
-        if (timeLeft ==0 )
+        if(GM.GameState == Enum.GameState.GamePhaseP1Run || GM.GameState == Enum.GameState.GamePhaseP2Run)
         {
+            if(CRRunning == false)
+            {
+                StartCoroutine("LoseTime");
+            }
+            countdown.text = ("" + timeLeft);
+            if (timeLeft == 0)
+            {
+                StopCoroutine("LoseTime");
+                CRRunning = false;
+                GM.RunnerWin = true;
+                GM.GameState = Enum.GameState.EndRound;
+            }
+        }
+        else
+        {
+            timeLeft = 30;
             StopCoroutine("LoseTime");
         }
     }
+            
     //Simple 
     IEnumerator LoseTime()
     {
+        CRRunning = true;
         while (true)
         {
             yield return new WaitForSeconds(1);
