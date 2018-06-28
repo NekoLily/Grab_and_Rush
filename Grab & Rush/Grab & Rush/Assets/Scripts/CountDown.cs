bc4c1b003005950a0576dfa6e165c4d36325d987
DESKTOP-1KAV99S\cleme <clement.lavieville@gmail.com>
2018-06-28 09:11:34 +0200

    Timer

---
+++ Grab & Rush/Grab & Rush/Assets/Scripts/CountDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class CountDown : MonoBehaviour
+{
+    public int timeLeft = 60;
+    public Text countdown;
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1;
+    }
+    void Update()
+    {
+        countdown.text = ("" + timeLeft);
+        if (timeLeft ==0 )
+        {
+            StopCoroutine("LoseTime");
+        }
+    }
+    //Simple 
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
